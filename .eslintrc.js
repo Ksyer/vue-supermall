71026e4efa85dc5500a9fbf0e2292939a66ad505
huatree <wang.yaohua@foxmail.com>
2021-05-10 01:05:42 +0800

    🐎 ci(eslint): 修改配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 0,
-    'no-trailing-spaces': 0
+    'no-trailing-spaces': 0,
+    'spaced-comment': 0
   }
 }
