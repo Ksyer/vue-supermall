b3ebcfa7f455b55aad9198f9d4cc8a859a778b40
huatree <wang.yaohua@foxmail.com>
2021-05-11 00:25:06 +0800

    ✨ feat(首页): 编写商品列表组件

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="v in goods" :key="v.iid" :goods-item="v" />
+  </div>
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem.vue'
+export default {
+  name: 'GoodsList',
+  components: { GoodsListItem },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+
+  padding: 2px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,115 +1,19 @@
 <template>
   <div class="home">
+    <!-- 顶部导航 -->
     <navbar class="home-nav">
       <div slot="center">购物街</div>
     </navbar>
+    <!-- 轮播图 -->
     <home-swiper :banners="banners" />
+    <!-- 推荐 -->
     <recommend :recommends="recommends" />
+    <!-- 本周流行 -->
     <feature-view />
-    <tab-control :titles="titles" />
-
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-      <li>列表51</li>
-      <li>列表52</li>
-      <li>列表53</li>
-      <li>列表54</li>
-      <li>列表55</li>
-      <li>列表56</li>
-      <li>列表57</li>
-      <li>列表58</li>
-      <li>列表59</li>
-      <li>列表60</li>
-      <li>列表61</li>
-      <li>列表62</li>
-      <li>列表63</li>
-      <li>列表64</li>
-      <li>列表65</li>
-      <li>列表66</li>
-      <li>列表67</li>
-      <li>列表68</li>
-      <li>列表69</li>
-      <li>列表70</li>
-      <li>列表71</li>
-      <li>列表72</li>
-      <li>列表73</li>
-      <li>列表74</li>
-      <li>列表75</li>
-      <li>列表76</li>
-      <li>列表77</li>
-      <li>列表78</li>
-      <li>列表79</li>
-      <li>列表80</li>
-      <li>列表81</li>
-      <li>列表82</li>
-      <li>列表83</li>
-      <li>列表84</li>
-      <li>列表85</li>
-      <li>列表86</li>
-      <li>列表87</li>
-      <li>列表88</li>
-      <li>列表89</li>
-      <li>列表90</li>
-      <li>列表91</li>
-      <li>列表92</li>
-      <li>列表93</li>
-      <li>列表94</li>
-      <li>列表95</li>
-      <li>列表96</li>
-      <li>列表97</li>
-      <li>列表98</li>
-      <li>列表99</li>
-      <li>列表100</li>
-    </ul>
+    <!-- 流行/新款/精选 -->
+    <tab-control :titles="titles" @tab-click="tabClick" />
+    <!-- 商品列表 -->
+    <goods-list :goods="showGoods" />
   </div>
 </template>
 
@@ -118,6 +22,7 @@
 ---------------------------------------------------------------- */
 import Navbar from 'components/common/navbar/Navbar'
 import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
 
 /* 子组件
 ---------------------------------------------------------------- */
@@ -127,7 +32,7 @@ import FeatureView from './FeatureView'
 
 /* 功能模块
 ---------------------------------------------------------------- */
-import { reqMulitdata } from 'api'
+import { reqMulitdata, reqHomeGoods } from 'api'
 
 export default {
   name: 'Home',
@@ -136,21 +41,66 @@ export default {
     TabControl,
     HomeSwiper,
     Recommend,
-    FeatureView
+    FeatureView,
+    GoodsList
   },
   data() {
     return {
       banners: [],
       recommends: [],
-      titles: ['流行', '新款', '精选']
+      titles: ['流行', '新款', '精选'],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      titleType: 'pop'
     }
   },
-  async created() {
-    const {
-      data: { banner, recommend }
-    } = await reqMulitdata()
-    this.banners = banner.list
-    this.recommends = recommend.list
+  methods: {
+    async getMulitData() {
+      const {
+        data: { banner, recommend }
+      } = await reqMulitdata()
+      this.banners = banner.list
+      this.recommends = recommend.list
+    },
+    async getHomeGoods(titleType) {
+      const page = this.goods[titleType].page + 1
+      const { success, data } = await reqHomeGoods({ type: titleType, page })
+      if (success) {
+        this.goods[titleType].list.push(...data.list)
+        this.goods[titleType].page += 1
+      }
+    },
+
+    /**
+     * 点击流行/新款/精选获取对应name值
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.titleType = 'pop'
+          break
+        case 1:
+          this.titleType = 'new'
+          break
+        default:
+          this.titleType = 'sell'
+          break
+      }
+    }
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.titleType].list
+    }
+  },
+  created() {
+    this.getMulitData()
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
   }
 }
 </script>
