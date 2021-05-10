fcf814e7c146182d50370ddb87be7b8d453edd1b
huatree <wang.yaohua@foxmail.com>
2021-05-10 23:00:02 +0800

    🐎 ci(vue.config): 配置source map

--- vue.config.js
+++ vue.config.js
@@ -4,27 +4,28 @@ const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
   chainWebpack: config => {
+    config.devtool('eval-source-map')
     config.resolve.alias
-    .set('@', resolve('src'))
-    .set('api', resolve('src/api'))
-    .set('assets', resolve('src/assets'))
-    .set('components', resolve('src/components'))
-    .set('store', resolve('src/store'))
-    .set('views', 'src/views')
-    
+      .set('@', resolve('src'))
+      .set('api', resolve('src/api'))
+      .set('assets', resolve('src/assets'))
+      .set('components', resolve('src/components'))
+      .set('store', resolve('src/store'))
+      .set('views', 'src/views')
+
     // 彻底懒加载
     // config.plugins.delete('prefetch')
   },
   devServer: {
     open: true,
-    // proxy: {
-    //   '/api': {
-    //     // target: '', 
-    //     changeOrigin: true,
-    //     pathRewrite: {
-    //       '^/api': ''
-    //     }
-    //   }
-    // }
+    proxy: {
+      '/api': {
+        target: 'http://152.136.185.210:7878',
+        changeOrigin: true
+        // pathRewrite: {
+        //   '^/api': ''
+        // }
+      }
+    }
   }
 }
